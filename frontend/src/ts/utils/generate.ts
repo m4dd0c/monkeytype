4ffd4be45e732efb80ae92c8db32971076ffaf8f
Manish Suthar <manishsuthar078@gmail.com>
2025-04-24 00:20:33 +0530

    feat: GetText.getRussian fn added

--- frontend/src/ts/utils/generate.ts
+++ frontend/src/ts/utils/generate.ts
@@ -52,6 +52,95 @@ export function getArabic(): string {
 }
 
 /**
+ * Generates a russian string (Length: Range 1 to 5)
+ * @returns The generated russian string.
+ */
+export function getRussian(): string {
+  const randLen = randomIntFromRange(1, 5);
+  const russianLetters = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "А",
+    "Б",
+    "В",
+    "Г",
+    "Д",
+    "Е",
+    "Ё",
+    "Ж",
+    "З",
+    "И",
+    "Й",
+    "К",
+    "Л",
+    "М",
+    "Н",
+    "О",
+    "П",
+    "Р",
+    "С",
+    "Т",
+    "У",
+    "Ф",
+    "Х",
+    "Ц",
+    "Ч",
+    "Ш",
+    "Щ",
+    "Ъ",
+    "Ы",
+    "Ь",
+    "Э",
+    "Ю",
+    "Я",
+    "а",
+    "б",
+    "в",
+    "г",
+    "д",
+    "е",
+    "ё",
+    "ж",
+    "з",
+    "и",
+    "й",
+    "к",
+    "л",
+    "м",
+    "н",
+    "о",
+    "п",
+    "р",
+    "с",
+    "т",
+    "у",
+    "ф",
+    "х",
+    "ц",
+    "ч",
+    "ш",
+    "щ",
+    "ъ",
+    "ы",
+    "ь",
+    "э",
+    "ю",
+    "я",
+  ];
+  return Array.from({ length: randLen }, () => {
+    return Arrays.randomElementFromArray(russianLetters);
+  }).join("");
+}
+
+/**
  * Generates a random hexadecimal string between 1 and 8 bytes.
  * @returns The generated hexadecimal string.
  */
